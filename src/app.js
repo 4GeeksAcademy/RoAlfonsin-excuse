54add68bf1002fb9928e4519ae1fdcce78ad4fae
RoAlfonsin <130806097+RoAlfonsin@users.noreply.github.com>
2023-05-29 20:14:10 -0600

    Clean code app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,12 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 import { Button } from "bootstrap";
 
-function generate_excuse() {
+function randomIndexSelector(wordsArray){
+  let arrayLength = wordsArray.length;
+  return parseInt(Math.random() * 9991) % arrayLength;
+}
+  
+function generateExcuse() {
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
@@ -17,25 +22,25 @@ function generate_excuse() {
     "during my lunch",
     "while I was praying"
   ];
-  let randwho = parseInt(Math.random() * 9991) % who.length;
-  let randaction = parseInt(Math.random() * 9991) % action.length;
-  let randwhat = parseInt(Math.random() * 9991) % what.length;
-  let randwhen = parseInt(Math.random() * 9991) % when.length;
+  let randWho = randomIndexSelector(who);
+  let randAction = randomIndexSelector(action);
+  let randWhat = randomIndexSelector(what);
+  let randWhen = randomIndexSelector(when);
   let frase =
-    who[randwho] +
+    who[randWho] +
     " " +
-    action[randaction] +
+    action[randAction] +
     " " +
-    what[randwhat] +
+    what[randWhat] +
     " " +
-    when[randwhen];
+    when[randWhen];
   document.querySelector("#excuse").innerHTML = frase;
 }
 
 window.onload = function() {
   //write your code here
-  generate_excuse();
+  generateExcuse();
 };
 
-var button = document.getElementById("my_button");
-button.addEventListener("click", generate_excuse);
+var button = document.getElementById("myButton");
+button.addEventListener("click", generateExcuse);
